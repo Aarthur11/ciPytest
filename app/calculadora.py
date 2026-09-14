ba457ae45acd9f02bb4181b368cce2b23bc9d03d
Aluno Alagoinhas <aluno.alagoinhas@senaibahia.com.br>
2026-09-14 14:31:53 -0300

    Cria projejto com pytest

---
+++ app/calculadora.py
@@ -0,0 +1,16 @@
+def somar(a,b):
+    return a + b
+
+def subtrair(a,b):
+    return a - b
+
+def multiplicar(a,b):
+    return a * b
+
+def dividir(a,b):
+    return a / b
+
+
+def calcular_desconto(preco,percentual):
+    desconto = preco * percentual /100
+    return preco - desconto
---
+++ tests/test_calculadora.py
@@ -0,0 +1,27 @@
+from app .calculadora import(
+    somar,
+    subtrair,
+    calcular_desconto,
+    multiplicar,
+    dividir
+)
+
+def test_somar():
+    resultado = somar(2,3)
+    assert resultado == 5
+
+def test_subtrair():
+    resultado = subtrair(10,4)
+    assert resultado == 6
+
+def test_calcular_desconto():
+    resultado = calcular_desconto(100,10)
+    assert resultado == 90
+
+def test_multiplicar():
+    resultado = multiplicar(10,1)
+    assert resultado == 10
+
+def test_dividir():
+    resultado = dividir(16,8)
+    assert resultado == 2

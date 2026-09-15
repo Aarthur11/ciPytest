74a45d8b21b621962f00beb4262ebc81d9fef7a4
ARTHUR <midoria755@gmail.com>
2026-09-15 15:53:39 -0300

    Adiciona multiplicacao (eu ja tinha criado multiplicacao antes)

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -19,7 +19,7 @@ def test_calcular_desconto():
     assert resultado == 90
 
 def test_multiplicar():
-    resultado = multiplicar(4,4)
+    resultado = multiplicar(4,5)
     assert resultado == 20
 def test_dividir():
     resultado = dividir(16,8)
